8d9560a586fcb6252e9e7ba11241b3cfff4d65ab
saxonsgrammar <gramatikvlad1986@gmail.com>
2023-06-30 17:01:03 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(20, 148);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(498, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "У вас есть 20 секунд. Сделайте как можно больше кликов за это время.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 45);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(313, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(137, 45);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Max. result";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 254);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clicker";
+            this.TopMost = true;
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        Timer timer = new Timer();
+        List<int> list = new List<int>();
+        int clicks = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Enabled = false;
+            timer.Tick += new EventHandler(Show_Clicks);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clicks = 0;
+            button1.Enabled = false;
+            button2.Enabled = false;
+            timer.Interval = 20000; 
+            timer.Start();
+            timer.Enabled = true;
+        }
+        private void Show_Clicks(object sender, EventArgs e)
+        {
+            timer.Stop();
+            MessageBox.Show("Кол-во кликов: " + clicks + "");
+            timer.Enabled = false;
+            list.Add(clicks);
+            button1.Enabled = true;
+            button2.Enabled = true;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Лучший результат: " + list.Max());
+        }
+
+        private void Form1_MouseClick_1(object sender, MouseEventArgs e)
+        {
+            if (timer.Enabled) clicks++;
+        }
+    }
+}
